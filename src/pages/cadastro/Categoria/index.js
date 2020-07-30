37984beb2918ce58cac13ee2193445e49fd447aa
edsondesennajunior <edsonsjr62@gmail.com>
2020-07-30 20:40:47 -0300

    Creating form field component and using state

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import Scaffold from '../../../components/Scaffold';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function CategoryForm() {
-    return (
-        <Scaffold>
-            <h1>Cadastro de Categoria</h1>
+import Scaffold from '../../../components/Scaffold';
+import FormField from '../../../components/FormField';
 
-            <Link to="/">Ir para Home</Link>
-        </Scaffold>
-    )
+function CategoryForm() {
+  const initialValues = {
+    name: '',
+    description: '',
+    color: '',
+  };
+  const [categories, setCategories] = useState([]);
+  const [formValues, setValues] = useState(initialValues);
+
+  function setValue(key, value) {
+    setValues({ ...formValues, [key]: value });
+  }
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+    setValue(event.target.getAttribute('name'), value);
+  }
+
+  function handleSubmitEvent(event) {
+    event.preventDefault();
+    setCategories([...categories, formValues]);
+    setValues(initialValues);
+  }
+
+  return (
+    <Scaffold>
+      <h1>Cadastro de Categoria: {formValues.name} </h1>
+
+      <form onSubmit={handleSubmitEvent}>
+        <FormField label='Nome:' value={formValues.name} onChange={handleInputChange} />
+
+        <FormField label='Descrição:' type="textarea" value={formValues.name} onChange={handleInputChange} />
+
+        <FormField label='Cor:' type='color' value={formValues.name} onChange={handleInputChange} />
+
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categories.map((category, index) => {
+          return <li key={`${category.name}${index}`}>{category.name}</li>;
+        })}
+      </ul>
+
+      <Link to='/'>Ir para Home</Link>
+    </Scaffold>
+  );
 }
 
 export default CategoryForm;
